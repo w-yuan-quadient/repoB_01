6e338f33c683e66e10e0fee489ed848cf7d07511
w-yuan-quadient <w.yuan@quadient.com>
2026-07-14 10:29:15 -0700

    live demo

--- VectorMul/VectorMul.cpp
+++ VectorMul/VectorMul.cpp
@@ -5,6 +5,7 @@ void vector_mul(const double* a, const double* b, double* out, int size)
     // empty line comment
     // 2nd empty line comment
 	// 3nd empty line added on July 14, 2026
+    // now it's time to trigger it.
     for (int i = 0; i < size; ++i)
     {
         out[i] = a[i] * b[i];
