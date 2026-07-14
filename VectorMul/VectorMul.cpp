d5d0e4b509497c0b7ebf81e9888bd1f88eea6001
w-yuan-quadient <w.yuan@quadient.com>
2026-07-14 08:26:43 -0700

    add a empty line

--- VectorMul/VectorMul.cpp
+++ VectorMul/VectorMul.cpp
@@ -4,6 +4,7 @@ void vector_mul(const double* a, const double* b, double* out, int size)
 {
     // empty line comment
     // 2nd empty line comment
+	// 3nd empty line added on July 14, 2026
     for (int i = 0; i < size; ++i)
     {
         out[i] = a[i] * b[i];
