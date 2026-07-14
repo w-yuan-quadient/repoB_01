fed6d622271ab3a2d1521aa589a8a7a903f0b7d6
w-yuan-quadient <w.yuan@quadient.com>
2026-07-14 14:01:24 -0700

    add interval check 2

--- VectorMul/VectorMul.cpp
+++ VectorMul/VectorMul.cpp
@@ -10,6 +10,7 @@ void vector_mul(const double* a, const double* b, double* out, int size)
     // 2nd try.
     // 3rd try.
     // interval check 1
+    // interval check 2
     for (int i = 0; i < size; ++i)
     {
         out[i] = a[i] * b[i];
