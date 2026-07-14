c86c994958b919f295ed52c87fde277d78ae3405
w-yuan-quadient <w.yuan@quadient.com>
2026-07-14 13:47:19 -0700

    add 2nd try

--- VectorMul/VectorMul.cpp
+++ VectorMul/VectorMul.cpp
@@ -7,6 +7,7 @@ void vector_mul(const double* a, const double* b, double* out, int size)
 	// 3nd empty line added on July 14, 2026
     // now it's time to trigger it.
     // 1st try.
+    // 2nd try.
     for (int i = 0; i < size; ++i)
     {
         out[i] = a[i] * b[i];
